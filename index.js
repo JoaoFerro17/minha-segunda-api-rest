8f84fb75b8dda9135bac192f1fe9bae2aa87fb75
João Victor Ferro <joaovictor.mafer@gmail.com>
2026-03-13 15:55:01 -0300

    feat: inserindo os requisitos pedidos na atividade da aula 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+const PORT = 3000;
 
 app.use(express.json());
 
@@ -21,7 +21,26 @@ app.get('/info', (req, res) => {
         autor: 'Seu Nome'
     });
 });
-
+app.get('/api/me', (req, res) => {
+    res.json({
+        nome: "João Victor Martins Ferro",
+        curso: "Ciência da Computação",
+        hobbies: ["Ler", "Assistir a filmes", "jogar"],
+        linguagens: ["Java"]
+    });
+});
+app.get('/api/data', (req, res) => {
+    res.json({
+        data_atual: new Date().toLocaleDateString('pt-BR'),
+        hora_atual: new Date().toLocaleTimeString('pt-BR')
+    });
+});
+app.get('/api/random', (req, res) => {
+    const numero = Math.floor(Math.random() * 100);
+    res.json({
+        numero_gerado: numero
+    });
+});
 app.listen(PORT, () => {
     console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 });
